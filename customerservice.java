2d58f8eaaab400d998f8431e8956721191c2acf2
Rizwan399 <96697513+Rizwan399@users.noreply.github.com>
2023-02-23 12:17:04 +0530

    RestAPI CRUD operations

---
+++ customerservice.java
@@ -0,0 +1,37 @@
+package com.example.demo.Customers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+	@Autowired
+	CustomerRepository repository;
+	
+	public String addCustomer(Customer customer) {
+		repository.save(customer);
+		return "Added";
+	}
+	
+	public List<Customer> getCustomer(){
+		return repository.findAll();
+	}
+
+	public Optional<Customer> getCustomerById(int id) {
+		// TODO Auto-generated method stub
+		return repository.findById(id);
+	}
+
+	public String updateCustomer(Customer customer) {
+		repository.save(customer);
+		return "Updated";
+	}
+
+	public String deleteCustomerById(int id) {
+		repository.deleteById(id);
+		return "Customer deleted";
+	}
+}
